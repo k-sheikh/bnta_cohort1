1337f084f6eb2b3660a0125cd100d0e91eeba53d
Michele Di Cosmo <micheledicosmo@vascosmi.com>
2021-04-14 13:07:44 +0200

    Add Vue's code seen in class

---
+++ Day 43/code/src/components/Weather.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h3>Day starts on
+            {{ weather 
+                ? Date(weather['First_UTC']).toLocaleString()
+                : 'Choose a day'
+            }}</h3>
+        <p>Season: {{ weather ? weather['Season'] : 'Choose a day' }}</p>
+        <p>Pressure: {{ weather ? weather['PRE'] : 'Choose a day' }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Weather',
+    props: ['weather']
+}
+</script>
+
+<style scoped>
+</style>
